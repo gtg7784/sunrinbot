d6c4700b6709e3bd5c323bd3bb05fab95e96192f
gtg7784 <gtg7784@naver.com>
2020-08-16 21:55:54 +0900

    add neis info (meal)

--- app.py
+++ app.py
@@ -1,14 +1,26 @@
 import os
-import random
+import datetime
+import neispy
 from flask import Flask, request
 from dotenv import load_dotenv
 from pymessenger.bot import Bot
 
 load_dotenv(verbose=True)
+
 app = Flask(__name__)
+
 ACCESS_TOKEN = os.getenv('ACCESS_TOKEN')
 VERIFY_TOKEN = os.getenv('VERIFY_TOKEN')
 bot = Bot(ACCESS_TOKEN)
+
+SCHOOL_NAME = "선린인터넷고등학교"
+
+API_TOKEN = os.getenv('OPEN_API_TOKEN')
+neis = neispy.SyncClient(KEY=API_TOKEN)
+school_info = neis.schoolInfo(SCHUL_NM=SCHOOL_NAME)
+
+AE = school_info.ATPT_OFCDC_SC_CODE  # 교육청 코드
+SE = school_info.SD_SCHUL_COD # 학교 코드
 
 @app.route('/', methods=['GET', 'POST'])
 def chat():
@@ -23,13 +35,12 @@ def chat():
         if message.get('message'):
           recipient_id = message['sender']['id']
           if message['message'].get('text'):
-            response_sent_text = get_message()
+            response_sent_text = get_meal(is_now=True)
             send_message(recipient_id, response_sent_text)
           if message['message'].get('attachments'):
-            response_sent_nontext = get_message()
+            response_sent_nontext = "attach"
             send_message(recipient_id, response_sent_nontext)
   return "Message Processed"
-
 
 @app.route('/test', methods=['GET'])
 def test():
@@ -40,13 +51,22 @@ def vertify_token(token):
     return request.args.get('hub.challenge')
   return 'Invalid Verification Token'
 
-def get_message():
-  sample_responses = ["You are stunning!", "We're proud of you.", "Keep on being you!", "We're greatful to know you :)"]
-  return random.choice(sample_responses)
+
+def choice_message(text):
+  selections = ['급식', ]
 
 def send_message(recipient_id, response):
   bot.send_text_message(recipient_id, response)
   return "success"
+
+def get_meal(date, is_now=False):
+  if is_now:
+    date = datetime.now().strftime('%Y%m%d')
+
+  meal_info = neis.mealServiceDietInfo(AE, SE, MLSV_YMD=date)
+
+  return meal_info
+
 
 if __name__ == "__main__":
   app.run(host='0.0.0.0', port=80)
